b5b2d808047957f1eebc833e50a45f7f3039ce2d
Robert Lohr <4004615+the-codeslinger@users.noreply.github.com>
2020-06-01 09:19:31 +0200

    Remove creation of custom PSQL image.

--- src/test-integration/resources/setup-postgres.sql
+++
@@ -1,3 +0,0 @@
-create database docker_db_testing_tests;
-create role docker_db_testing_tests with login nosuperuser inherit createdb nocreaterole noreplication password 'docker_db_testing_tests';
-grant create, connect on database docker_db_testing_tests to docker_db_testing_tests;
